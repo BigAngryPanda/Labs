26e0b10b7acf7d81d6fe683c1e47ee9cf573690d
BigAngryPanda <BigAngryPanda@gmail.com>
2020-05-22 22:26:35 +0300

    Separate tasks

--- lab_se_4/src/beans/InfoMBean.java
+++
@@ -1,7 +0,0 @@
-import javax.management.NotificationBroadcaster;
-
-public interface InfoMBean extends NotificationBroadcaster {
-    int getTotalPointsCount();
-    int getMissedPointsCount();
-    void addResult(boolean result);
-}
--- lab_se_4/src/beans/IntervalMBean.java
+++
@@ -1,4 +0,0 @@
-public interface IntervalMBean {
-    String getAvgInterval();
-    void addPoint();
-}
